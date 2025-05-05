1b5ea3ce4213f44e4d9d8f799fab6d3b46d89a60
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2025-05-05 22:46:42 +0530

    setup tailwind css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-    <h1>Empowering Businesses Through Smart Digital Solutions</h1>
-    <p>
-      At Bluemine Technologies, we specialize in driving business growth through strategic digital marketing and advanced software solutions...
-    </p>
-  </div>
-  )
-}
+    <div className="p-8 text-center bg-gray-50 min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-700 mb-4">
+        Empowering Businesses Through Smart Digital Solutions
+      </h1>
+      <p className="text-lg text-gray-700">
+        At Bluemine Technologies, we specialize in driving business growth through strategic digital marketing and advanced software solutions.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
