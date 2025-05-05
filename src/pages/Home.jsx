82ce8914a8a5f2dcd4f7139dded057fc6f651ad7
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2025-05-05 22:26:42 +0530

    router and setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    <h1>Empowering Businesses Through Smart Digital Solutions</h1>
+    <p>
+      At Bluemine Technologies, we specialize in driving business growth through strategic digital marketing and advanced software solutions...
+    </p>
+  </div>
+  )
+}
+
+export default Home
